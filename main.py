f8f1a9e0301677d5008ee0bf0fe40e8fed960be0
oinoirenigonasoindoisanfoidsn <140310847+oinoirenigonasoindoisanfoidsn@users.noreply.github.com>
2023-09-14 19:04:48 +1000

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,24 @@ def get_random_artist_album():
     firstArtistPage = urllib.urlopen(mainLink)
     formattedFirstArtistPage = BeautifulSoup(firstArtistPage, "html.parser")
     pageList = formattedFirstArtistPage.find('ul', {"class": 'pagelist'})
-    
+
     if pageList is None:
         print("\nLooks like an incorrect tag was entered! The program will now close.")
         return
 
     page_numbers = [int(li.string) for li in pageList if li.string.isnumeric()]
     max_page_number = max(page_numbers)
-    
+
     random_page_number = random.randint(1, max_page_number)
     url = f"{mainLink}?page={random_page_number}"
     artist_links = get_artist_links(url)
-    
+
     if not artist_links:
         return
 
     specific_artist_link = random.choice(artist_links)
     selected_album = randomly_select_album(specific_artist_link)
-    
+
     return selected_album
 
 def get_artist_links(url):
@@ -36,7 +36,7 @@ def get_artist_links(url):
     artistPage = urllib.urlopen(url)
     formattedArtistPage = BeautifulSoup(artistPage, "html.parser")
     allArtistList = formattedArtistPage.find('ul', {"class": 'item_list'})
-    
+
     if allArtistList is None:
         return artist_links
 
@@ -48,7 +48,7 @@ def get_artist_links(url):
 
 def randomly_select_album(specific_artist_link):
     artistAlbums = []
-    searchFor = r'\S*(\.com|\.net|\.uk)'
+    searchFor = r'\S*(\\.com|\\.net|\\.uk)'
     artistBaseLink = re.search(searchFor, specific_artist_link)
     unformattedArtistPage = urllib.urlopen(artistBaseLink.group(0))
     formattedArtistPage = BeautifulSoup(unformattedArtistPage, "html.parser")
@@ -67,7 +67,7 @@ def randomly_select_album(specific_artist_link):
 def main():
     for x in range(4):  # Download 4 albums
         album_link = get_random_artist_album()
-        
+
         if album_link:
             print(f"{x + 1}: {album_link}")
             dir_path = os.path.dirname(os.path.realpath(__file__))
