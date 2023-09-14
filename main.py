8204dbf18ac7f6edb5412ee035b6b95f1dd4eee0
oinoirenigonasoindoisanfoidsn <140310847+oinoirenigonasoindoisanfoidsn@users.noreply.github.com>
2023-09-14 18:54:10 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import urllib.request as urllib
 from bs4 import BeautifulSoup
-import numpy 
+import numpy
 import re
-import sys
 import os
-#import subprocess
 import webbrowser
 
+# Install required packages in Colab
+!pip install bandcamp-downloader
+
 # --------------------------------------------------------------------------------------
-# Get the HTML for the page of the provided link and store it in a Beautiful soup format
+# Get the HTML for the page of the provided link and store it in a Beautiful Soup format
 # --------------------------------------------------------------------------------------
 
 def allArtistsFirstPage(mainLink):
 
-	firstArtistPage = urllib.urlopen(mainLink)
+    firstArtistPage = urllib.urlopen(mainLink)
 
-	formattedFirstArtistPage = BeautifulSoup(firstArtistPage, "html.parser")
+    formattedFirstArtistPage = BeautifulSoup(firstArtistPage, "html.parser")
 
-	return formattedFirstArtistPage
+    return formattedFirstArtistPage
 
 # ------------------------------------------------------------------
 # Find the number of pages on the first page and randomly select one
@@ -25,132 +26,127 @@ def allArtistsFirstPage(mainLink):
 
 def numberOfPages(formattedFirstPage, mainLink):
 
-	numbers = []
-	previous = 0
+    numbers = []
+    previous = 0
 
-	pageList = formattedFirstPage.find('ul', {"class" : 'pagelist'})
+    pageList = formattedFirstPage.find('ul', {"class" : 'pagelist'})
 
-	if pageList == None:
-		print("\nLooks like an incorrect tag was entered! The program will now close.")
-		sys.exit()
+    if pageList == None:
+        print("\nLooks like an incorrect tag was entered! The program will now close.")
+        return None
 
-	# Get a list of page numbers that contains the last page (the largest number of pages that need to be searched)
-	for li in pageList:
-		item = li.string
-		
-		if item.isnumeric():
-			if item != (1 + previous):
-				numberOfPages = int(item)
-			else:
-				previous = item
+    # Get a list of page numbers that contains the last page (the largest number of pages that need to be searched)
+    for li in pageList:
+        item = li.string
 
-	limit = numberOfPages + 1
+        if item.isnumeric():
+            if item != (1 + previous):
+                numberOfPages = int(item)
+            else:
+                previous = item
 
-	# Randomly choose the page number to select the artist from
-	randomPageNumber = numpy.random.randint(1, limit)
+    limit = numberOfPages + 1
 
-	url = mainLink + "?page=" + str(randomPageNumber)
+    # Randomly choose the page number to select the artist from
+    randomPageNumber = numpy.random.randint(1, limit)
 
-	return url
+    url = mainLink + "?page=" + str(randomPageNumber)
+
+    return url
 
 # --------------------------------------------------------------------------------------------
-# Create a list of all of the artists on the randomly selected page and randomly select one 
+# Create a list of all the artists on the randomly selected page and randomly select one
 # --------------------------------------------------------------------------------------------
 
 def randomlySelectArtist(url):
 
-	allArtistLinks = []
-	artistPage = urllib.urlopen(url)
+    allArtistLinks = []
+    artistPage = urllib.urlopen(url)
 
-	formattedArtistPage = BeautifulSoup(artistPage, "html.parser")
+    formattedArtistPage = BeautifulSoup(artistPage, "html.parser")
 
-	# Find the unordered list where class = "item_list", this contains the relevant code
-	allArtistList = formattedArtistPage.find('ul', {"class" : 'item_list'})
+    # Find the unordered list where class = "item_list", this contains the relevant code
+    allArtistList = formattedArtistPage.find('ul', {"class" : 'item_list'})
 
-	# For the anchor tag in each li element in the ul tag
-	for listItem in allArtistList.findAll("a"):
-	# Get the link in the href
-		artistHomepage = listItem.get("href")
-	# Appending /music to the link will insure that all of the artists albums will be presented uniformly to the program
-		allArtistLinks.append(artistHomepage)
+    # For the anchor tag in each li element in the ul tag
+    for listItem in allArtistList.findAll("a"):
+        # Get the link in the href
+        artistHomepage = listItem.get("href")
+        # Appending /music to the link will ensure that all of the artist's albums will be presented uniformly to the program
+        allArtistLinks.append(artistHomepage)
 
-	length = len(allArtistLinks)
-	randomIndex = numpy.random.randint(0, length)
+    length = len(allArtistLinks)
+    randomIndex = numpy.random.randint(0, length)
 
-	specificArtistLink = allArtistLinks[randomIndex]
+    specificArtistLink = allArtistLinks[randomIndex]
 
-	return specificArtistLink
+    return specificArtistLink
 
 # ---------------------------------------------------------------------------------------------------
-# Find all of the albums an artist has created, randomly select one and present the url to the user
+# Find all of the albums an artist has created, randomly select one, and present the URL to the user
 # ---------------------------------------------------------------------------------------------------
 
 def randomlySelectAlbum(specificArtistLink):
-	
-	artistAlbums = []
-	
-	searchFor = r'\S*(\.com|\.net|\.uk)'
-	artistBaseLink = re.search(searchFor, specificArtistLink)
 
-	unformattedArtistPage = urllib.urlopen(artistBaseLink.group(0))
+    artistAlbums = []
 
-	formattedArtistPage = BeautifulSoup(unformattedArtistPage, "html.parser")
+    searchFor = r'\S*(\.com|\.net|\.uk)'
+    artistBaseLink = re.search(searchFor, specificArtistLink)
 
-	albumSection = formattedArtistPage.find('div', {"class" : 'leftMiddleColumns'})
+    unformattedArtistPage = urllib.urlopen(artistBaseLink.group(0))
 
-	# Getting the section of code that indicates if the artist has one or multiple albums
-	albumNumberCheck = formattedArtistPage.find('div', {"class" : 'trackView'})
+    formattedArtistPage = BeautifulSoup(unformattedArtistPage, "html.parser")
 
-	# If the artist has more than one album
-	if (albumNumberCheck == None):
+    albumSection = formattedArtistPage.find('div', {"class" : 'leftMiddleColumns'})
 
-		for listItem in albumSection.findAll("a"):
-			albumSubLink = listItem.get("href")
-			artistAlbums.append(artistBaseLink.group(0) + albumSubLink)
+    # Getting the section of code that indicates if the artist has one or multiple albums
+    albumNumberCheck = formattedArtistPage.find('div', {"class" : 'trackView'})
 
-		randomIndex = numpy.random.randint(0, len(artistAlbums))
+    # If the artist has more than one album
+    if (albumNumberCheck == None):
 
-		selectedAlbum = artistAlbums[randomIndex]
+        for listItem in albumSection.findAll("a"):
+            albumSubLink = listItem.get("href")
+            artistAlbums.append(artistBaseLink.group(0) + albumSubLink)
 
-		return selectedAlbum
+        randomIndex = numpy.random.randint(0, len(artistAlbums))
 
-	# Else the artist has only one album
-	else:
-		return specificArtistLink
+        selectedAlbum = artistAlbums[randomIndex]
+
+        return selectedAlbum
+
+    # Else the artist has only one album
+    else:
+        return specificArtistLink
 
 # ------------------------------------------------------------------------
-# Get user input if they want a random album or one from a specific tag 
+# Get user input if they want a random album or one from a specific tag
 # ------------------------------------------------------------------------
 
 def main():
-    if len(sys.argv) >= 2:
-        try:
-            amount = int(sys.argv[1])
-        except ValueError:
-            print("That was an invalid input! Program will now close.")
-            sys.exit()
-    else:
-        amount = input("How many albums do you want to download?: ")
+    amount = int(input("How many albums do you want to download?: "))
 
-        try:
-            amount = int(amount)
-        except ValueError:
-            print("That was an invalid input! Program will now close.")
-            sys.exit()
+    # Create a 'music' directory in the same location as the script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    music_dir = os.path.join(script_dir, "music")
+    os.makedirs(music_dir, exist_ok=True)
 
     for x in range(amount):
         mainLink = "https://bandcamp.com/artist_index"
 
         formattedFirstPage = allArtistsFirstPage(mainLink)
         url = numberOfPages(formattedFirstPage, mainLink)
+
+        if url is None:
+            continue  # Skip to the next iteration if there was an error
+
         artistLink = randomlySelectArtist(url)
         albumLink = randomlySelectAlbum(artistLink)
 
-        print(str(x+1) + ": " + albumLink)
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        #webbrowser.open(albumLink)
-        os.system("bandcamp-dl --base-dir=\"{}\\music\"".format(dir_path) + " --full-album --embed-art --template=\"%{album}/%{track} - %{title}\"" + " {}".format(albumLink))
-		
+        print(str(x + 1) + ": " + albumLink)
+
+        # Use the bandcamp-dl command to download the album and save it in the 'music' directory
+        os.system(f"bandcamp-dl --base-dir=\"{music_dir}\" --full-album --embed-art --template=\"%{{album}}/%{{track}} - %{{title}}\" {albumLink}")
 
 if __name__ == "__main__":
     main()
